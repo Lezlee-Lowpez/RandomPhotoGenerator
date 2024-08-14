a406644fc231fcb547962f05fe4f4fa356834d49
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-08-14 13:22:39 -0700

    Initial Commit

---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Lesley Lopez on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
