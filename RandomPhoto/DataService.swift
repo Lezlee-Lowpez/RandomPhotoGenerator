9009a39e06dda5ea1ddfe52f618d340756ae4b00
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-08-14 14:01:55 -0700

    -set up config and gitignore file - add DataService and altered info list

---
+++ RandomPhoto/DataService.swift
@@ -0,0 +1,13 @@
+//
+//  DataService.swift
+//  RandomPhoto
+//
+//  Created by Lesley Lopez on 8/14/24.
+//
+
+import Foundation
+
+struct DataService {
+    
+    var url = "https://api.unsplash.com/photos/random"
+}
